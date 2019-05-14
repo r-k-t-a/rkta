dc9f36b01bde2e673655dc294261c2c8202f27ce
pavel.brylev <pavel.brylev@supremedesign.ru>
2019-05-14 10:47:17 +0300

    чистка кеша docz

--- .docz/app/imports.js
+++
@@ -1,8 +0,0 @@
-export const imports = {
-  'index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "index" */ 'index.mdx'),
-  'packages/ui/src/Provider/Provider.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "packages-ui-src-provider-provider" */ 'packages/ui/src/Provider/Provider.mdx'
-    ),
-}
