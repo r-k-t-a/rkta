96371589ab1d1b62583fa6df02425ada8218b797
pavel.brylev <pavel.brylev@supremedesign.ru>
2019-05-16 14:09:10 +0300

    чистка

--- packages/ui/src/Provider/Context.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
-const defaultTheme_1 = require("./theme/defaultTheme");
-exports.default = react_1.createContext({ theme: defaultTheme_1.default });
-//# sourceMappingURL=Context.js.map
--- packages/ui/src/Provider/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Provider_1 = require("./Provider");
-exports.default = Provider_1.default;
-//# sourceMappingURL=index.js.map
