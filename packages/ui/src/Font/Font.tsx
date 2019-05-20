4672fd0eedf042f4d4045325af66fa4ed5b4bb46
Sergei Droganov <serge@droganov.ru>
2019-05-20 21:26:40 +0300

    Strict typescript

--- packages/ui/src/Font/Font.tsx
+++ packages/ui/src/Font/Font.tsx
@@ -4,11 +4,8 @@ import Atom from '../Atom';
 import themed from '../util/themed';
 import { FontProps } from './FontTypes';
 
-const Font = ({ children, css, element, ...rest }: FontProps): React.ReactNode =>
-  React.createElement(Atom, { ...rest, element, css }, children);
-// <Atom {...rest} element={element} css={css}>
-//   {children}
-// </Atom>
+const Font = ({ children, element, ...rest }: FontProps): React.ReactNode =>
+  React.createElement(Atom, { ...rest, element }, children);
 
 Font.defaultProps = {
   children: null,
