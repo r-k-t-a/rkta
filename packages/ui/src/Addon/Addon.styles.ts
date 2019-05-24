ca872d94cd0b1ee11db443962804484636a8a108
pavel.brylev <pavel.brylev@supremedesign.ru>
2019-05-24 15:22:37 +0300

    Addon

---
+++ packages/ui/src/Addon/Addon.styles.ts
@@ -0,0 +1,18 @@
+import { CssRkta } from '../Provider/theme/theme.types';
+
+export const initialStyle: CssRkta = {
+  display: 'block',
+  padding: '0 16px',
+};
+
+export const fitAll: CssRkta = {
+  padding: 0,
+};
+
+export const fitLeft: CssRkta = {
+  paddingLeft: 0,
+};
+
+export const fitRight: CssRkta = {
+  paddingRight: 0,
+};
