0fe26403d674a42d6686ce78b5cea8e1d51a38d4
pavel.brylev <pavel.brylev@supremedesign.ru>
2019-09-17 09:09:23 +0300

    wrap button with forwardRef

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { SFC, ReactElement } from 'react';
+import { forwardRef, SFC, ReactElement } from 'react';
 import { jsx } from '@emotion/core';
 
 import Spinner from '../Spinner';
@@ -11,32 +11,37 @@ import { spinnerCss } from './Button.styles';
 import Ripple from './Ripple';
 import useRipple from './Ripple/useRipple';
 
-const Button: SFC<ButtonProps> = ({
-  noRipple,
-  children,
-  composition = ['Button', 'Paper', 'Addon', 'Text'],
-  spinnerProps,
-  ...rest
-}: ButtonProps): ReactElement => {
-  const { applyStyles, getElement } = useProviderContext();
-  const [nextProps, Element] = applyStyles(
-    { element: 'button', normal: true, button: true, ...rest },
-    ...composition,
-  );
-  const [rippleProps, buttonProps] = useRipple(nextProps);
-  const SpinnerWrapper = getElement('span', {});
-  return (
-    <Element {...nextProps} {...buttonProps}>
-      {children}
-      {!noRipple && <Ripple {...rippleProps} />}
-      {rest.busy && (
-        <SpinnerWrapper css={spinnerCss}>
-          <Spinner color={rest.color} {...spinnerProps} />
-        </SpinnerWrapper>
-      )}
-    </Element>
-  );
-};
+const Button: SFC<ButtonProps> = forwardRef(
+  (
+    {
+      noRipple,
+      children,
+      composition = ['Button', 'Paper', 'Addon', 'Text'],
+      spinnerProps,
+      ...rest
+    }: ButtonProps,
+    ref,
+  ): ReactElement => {
+    const { applyStyles, getElement } = useProviderContext();
+    const [nextProps, Element] = applyStyles(
+      { element: 'button', normal: true, button: true, ...rest },
+      ...composition,
+    );
+    const [rippleProps, buttonProps] = useRipple(nextProps);
+    const SpinnerWrapper = getElement('span', {});
+    return (
+      <Element {...nextProps} {...buttonProps} ref={ref}>
+        {children}
+        {!noRipple && <Ripple {...rippleProps} />}
+        {rest.busy && (
+          <SpinnerWrapper css={spinnerCss}>
+            <Spinner color={rest.color} {...spinnerProps} />
+          </SpinnerWrapper>
+        )}
+      </Element>
+    );
+  },
+);
 
 Button.defaultProps = {
   busy: false,
