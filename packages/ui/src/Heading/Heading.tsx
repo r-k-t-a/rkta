de4c74d292771ae106fc4c4a7c9e87d5e304c3cf
Sergei Droganov <serge@droganov.ru>
2019-09-25 20:49:10 +0300

    override heading props with text props

--- packages/ui/src/Heading/Heading.tsx
+++ packages/ui/src/Heading/Heading.tsx
@@ -8,7 +8,7 @@ import { HeadingProps } from './Heading.defs';
 const getHeadingProps = (level: number, rest: Partial<HeadingProps>): TextProps => {
   if (level >= 1 && level <= 6) {
     const element = `h${level}` as React.ElementType;
-    return { element, ...rest, [element as string]: true };
+    return { element, [element as string]: true, ...rest };
   }
   return { element: 'header', ...rest };
 };
