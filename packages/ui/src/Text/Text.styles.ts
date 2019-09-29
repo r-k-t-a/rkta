9b7e1726b52733728ff5b2deeadfc9de7bd54fc4
droganov <serge@droganov.ru>
2019-09-29 15:23:25 +0300

    wrap text styles to css

--- packages/ui/src/Text/Text.styles.ts
+++ packages/ui/src/Text/Text.styles.ts
@@ -1,153 +1,154 @@
-import { Color, CssRkta, RktaTheme } from '../Provider/theme/theme.defs';
+import { css } from '@emotion/core';
+import { Color, CssEmotion, RktaTheme } from '../Provider/theme/theme.defs';
 
-export const color = (theme: RktaTheme, props: { color: Color }): CssRkta => ({
-  color: theme.color[props.color] || props.color,
-});
+export const color = (theme: RktaTheme, props: { color: Color }): CssEmotion => css`
+  color: ${theme.color[props.color] || props.color};
+`;
 
-export const sans: CssRkta = {
-  fontFamily: '"San Francisco", "Helvetica Neue", "Roboto", "Segoe UI", sans-serif',
-};
+export const sans: CssEmotion = css`
+  font-family: 'San Francisco', 'Helvetica Neue', 'Roboto', 'Segoe UI', sans-serif;
+`;
 
-export const serif: CssRkta = {
-  fontFamily: '"Playfair Display", Bookman, Palatino, Georgia, serif',
-};
+export const serif: CssEmotion = css`
+  font-family: 'Playfair Display', Bookman, Palatino, Georgia, serif;
+`;
 
-export const monospace: CssRkta = {
-  fontFamily:
-    '"Lucida Sans Typewriter", "Lucida Console", "Consolas", "Andale Mono", "Monaco", "Courier New", Courier, monospace',
-};
+export const monospace: CssEmotion = css`
+  font-family: 'Lucida Sans Typewriter', 'Lucida Console', 'Consolas', 'Andale Mono', 'Monaco',
+    'Courier New', Courier, monospace;
+`;
 
-export const uppercase: CssRkta = {
-  textTransform: 'uppercase',
-  letterSpacing: '0.75px',
-};
+export const uppercase: CssEmotion = css`
+  text-transform: uppercase;
+  letter-spacing: 0.75px;
+`;
 
-export const baseline: CssRkta = {
-  margin: 0,
-  padding: 0,
-};
+export const baseline: CssEmotion = css`
+  margin: 0;
+  padding: 0;
+`;
 
-export const muted: CssRkta = {
-  opacity: 0.56,
-  userSelect: 'none',
-};
+export const muted: CssEmotion = css`
+  opacity: 0.56;
+  user-select: none;
+`;
 
-export const center: CssRkta = {
-  textAlign: 'center',
-};
+export const center: CssEmotion = css`
+  text-align: center;
+`;
 
-export const etched: CssRkta = {
-  textShadow: '0 1px rgba(255, 255, 255, 0.24)',
-};
+export const etched: CssEmotion = css`
+  text-shadow: 0 1px rgba(255, 255, 255, 0.24);
+`;
 
-export const readOnly: CssRkta = {
-  pointerEvents: 'none',
-  userSelect: 'none',
-};
+export const readOnly: CssEmotion = css`
+  pointer-events: none;
+  user-select: none;
+`;
 
-export const nowrap: CssRkta = {
-  whiteSpace: 'nowrap',
-  textOverflow: 'ellipsis',
-  overflow: 'hidden',
-};
+export const nowrap: CssEmotion = css`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
 
-export const wrapLongLines: CssRkta = {
-  overflowWrap: 'break-word',
-  wordWrap: 'break-word',
-};
+export const wrapLongLines: CssEmotion = css`
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+`;
 
-export const hyphens: CssRkta = {
-  hyphens: 'auto',
-};
+export const hyphens: CssEmotion = css`
+  hyphens: auto;
+`;
 
 // size
-export const h1: CssRkta = {
-  fontSize: '96px',
-  fontWeight: 'lighter',
-  lineHeight: '96px',
-  letterSpacing: '-1.5px',
-};
+export const h1: CssEmotion = css`
+  font-size: 96px;
+  font-weight: lighter;
+  line-height: 96px;
+  letter-spacing: -1.5px;
+`;
 
-export const h2: CssRkta = {
-  fontSize: '60px',
-  fontWeight: 'lighter',
-  lineHeight: '60px',
-  letterSpacing: '-0.5px',
-};
+export const h2: CssEmotion = css`
+  font-size: 60px;
+  font-weight: lighter;
+  line-height: 60px;
+  letter-spacing: -0.5px;
+`;
 
-export const h3: CssRkta = {
-  fontSize: '48px',
-  fontWeight: 400,
-  lineHeight: '48px',
-};
+export const h3: CssEmotion = css`
+  font-size: 48px;
+  font-weight: 400;
+  line-height: 48px;
+`;
 
-export const h4: CssRkta = {
-  fontSize: '34px',
-  fontWeight: 400,
-  lineHeight: '40px',
-  letterSpacing: '0.25px',
-};
+export const h4: CssEmotion = css`
+  font-size: 34px;
+  font-weight: 400;
+  line-height: 40px;
+  letter-spacing: 0.25px;
+`;
 
-export const h5: CssRkta = {
-  fontSize: '24px',
-  fontWeight: 400,
-  lineHeight: '32px',
-};
+export const h5: CssEmotion = css`
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 32px;
+`;
 
-export const h6: CssRkta = {
-  fontSize: '22px',
-  fontWeight: 500,
-  lineHeight: '32px',
-  letterSpacing: '0.15px',
-};
+export const h6: CssEmotion = css`
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 32px;
+  letter-spacing: 0.15px;
+`;
 
-export const subtitle1: CssRkta = {
-  fontSize: '16px',
-  lineHeight: '24px',
-  fontWeight: 400,
-  letterSpacing: '0.15px',
-};
+export const subtitle1: CssEmotion = css`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: 0.15px;
+`;
 
-export const subtitle2: CssRkta = {
-  fontSize: '14px',
-  lineHeight: '20px',
-  fontWeight: 500,
-  letterSpacing: '0.1px',
-};
+export const subtitle2: CssEmotion = css`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  letter-spacing: 0.1px;
+`;
 
-export const body: CssRkta = {
-  fontSize: '16px',
-  fontWeight: 400,
-  letterSpacing: '0.15px',
-  lineHeight: '24px',
-  textRendering: 'optimizeLegibility',
-};
+export const body: CssEmotion = css`
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 0.15px;
+  line-height: 24px;
+  text-rendering: optimizeLegibility;
+`;
 
-export const body2: CssRkta = {
-  fontSize: '14px',
-  lineHeight: '20px',
-  fontWeight: 400,
-  letterSpacing: '0.1px',
-};
+export const body2: CssEmotion = css`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: 0.1px;
+`;
 
-export const button: CssRkta = {
-  fontSize: '14px',
-  lineHeight: '20px',
-  fontWeight: 500,
-  letterSpacing: '0.1px',
-  textDecoration: 'none',
-};
+export const button: CssEmotion = css`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  letter-spacing: 0.1px;
+  text-decoration: none;
+`;
 
-export const caption: CssRkta = {
-  fontSize: '12px',
-  lineHeight: '20px',
-  fontWeight: 400,
-  letterSpacing: '0.4px',
-};
+export const caption: CssEmotion = css`
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: 0.4px;
+`;
 
-export const overline: CssRkta = {
-  fontSize: '10px',
-  lineHeight: '20px',
-  fontWeight: 400,
-  letterSpacing: '1px',
-};
+export const overline: CssEmotion = css`
+  font-size: 10px;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: 1px;
+`;
