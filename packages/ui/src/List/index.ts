6307292d7140c4ba9ccdddda1f5038d75c395b4d
droganov <serge@droganov.ru>
2019-10-04 08:57:26 +0300

    add ListText to export

--- packages/ui/src/List/index.ts
+++ packages/ui/src/List/index.ts
@@ -2,3 +2,4 @@ export { default as List } from './List';
 export { default as ListButton } from './ListButton';
 export { default as ListItem } from './ListItem';
 export { default as ListTitle } from './ListTitle';
+export { default as ListText } from './ListText';
