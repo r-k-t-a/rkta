b04eed0fd4a5cf26a8184ccfbca2bd329aab2ba7
pavel.brylev <pavel.brylev@supremedesign.ru>
2019-10-29 14:43:08 +0300

    fix input prop types

--- packages/ui/src/Input/InputBase/InputBase.type.ts
+++ packages/ui/src/Input/InputBase/InputBase.type.ts
@@ -2,7 +2,7 @@ import { ReactNode, HTMLProps } from 'react';
 import { AddonOwnProps } from '../../Addon/Addon.type';
 
 export interface Props extends AddonOwnProps, HTMLProps<HTMLInputElement> {
-  active: boolean;
+  active?: boolean;
   big?: boolean;
   caption?: ReactNode;
   huge?: boolean;
