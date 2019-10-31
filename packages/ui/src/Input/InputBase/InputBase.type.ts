6422690b2657ba20b556fce961a3bafdd6714d1b
droganov <serge@droganov.ru>
2019-10-31 18:17:59 +0300

    feat: new input props
    readOnly, disabled, maxRows

--- packages/ui/src/Input/InputBase/InputBase.type.ts
+++ packages/ui/src/Input/InputBase/InputBase.type.ts
@@ -1,9 +1,17 @@
-import { HTMLProps, ReactNode } from 'react';
+import { FormEvent, HTMLProps, ReactNode } from 'react';
 import { AddonOwnProps } from '../../Addon/Addon.type';
 
 export interface Props extends AddonOwnProps, HTMLProps<HTMLInputElement | HTMLTextAreaElement> {
   active?: boolean;
   autoheight?: boolean;
   caption?: ReactNode;
+  disabled?: boolean;
+  maxRows?: number;
   multiline?: boolean;
+  onBlur?: (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  onChange?: (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  onFocus?: (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  placeholder?: string;
+  readOnly?: boolean;
+  value?: string;
 }
