1e8416a90996c06850d6d95e4e185f8c22ed9563
pavel.brylev <pavel.brylev@supremedesign.ru>
2019-11-12 11:02:26 +0300

    move entypo to separate package

---
+++ packages/entypo/src/icons/SquaredPlus.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+
+import { Svg } from '@rkta/ui';
+import { defaultIconSize } from '../constants';
+
+const SquaredPlus = <T extends {}>(props: T): ReactElement => (
+  <Svg size={defaultIconSize} {...props} viewBox="0 0 20 20">
+    <path d="M16,2H4C2.9,2,2,2.9,2,4v12c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2V4C18,2.9,17.1,2,16,2z M15,11h-4v4H9v-4H5V9h4V5h2v4h4V11z" />
+  </Svg>
+);
+
+export default SquaredPlus;
