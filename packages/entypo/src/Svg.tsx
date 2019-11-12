c576523929f5556aa93acd7d6e7d8abacbac6d07
Sergei Droganov <serge@droganov.ru>
2019-11-12 11:33:49 +0300

    Entypo to have it's own Svg

---
+++ packages/entypo/src/Svg.tsx
@@ -0,0 +1,35 @@
+/** @jsx jsx */
+import { ReactNode, SFC, useContext } from 'react';
+import { useTheme } from '@rkta/context';
+import { css, jsx } from '@emotion/core';
+
+interface Props {
+  children: ReactNode;
+  color?: string;
+  inline?: boolean;
+  size?: number | string;
+}
+
+const getCss = (theme, { inline = false, size = 24, color = '' }) => [
+  {
+    display: 'block',
+    fill: 'currentColor',
+    height: size,
+    verticalAlign: 'middle',
+    width: size,
+  },
+  inline && {
+    display: 'inline-block',
+  },
+  color && {
+    fill: theme.color[color] || color,
+  },
+];
+
+const TsElement: SFC<Props> = ({ children, ...rest }: Props): JSX.Element => {
+  const theme = useTheme();
+  const cssEmotion = getCss(rest, theme);
+  return <svg css={cssEmotion}>{children}</svg>;
+};
+
+export default TsElement;
