ffe52aa212ce1e720ec48ca0646dee8ecf2a228b
droganov <serge@droganov.ru>
2020-02-08 23:34:48 +0700

    Fix placehplder typings

--- src/packages/ui/src/Placeholder/Placeholder.tsx
+++ src/packages/ui/src/Placeholder/Placeholder.tsx
@@ -1,12 +1,12 @@
 /** @jsx jsx */
-import { ReactNode } from 'react';
+import { FC } from 'react';
 import { jsx } from '@emotion/core';
 
 import { useProviderContext } from '../Provider';
 import { Props } from './Placeholder.type';
 
-export const Placeholder = ({ children, element, ...rest }: Props): ReactNode => {
-  if (children) return children;
+export const Placeholder: FC<Props> = ({ children, element, ...rest }: Props): JSX.Element => {
+  if (children) return children as JSX.Element;
   const { applyStyles } = useProviderContext();
   const [nextProps, Element] = applyStyles({ element, ...rest }, 'Placeholder');
   return <Element {...nextProps} />;
