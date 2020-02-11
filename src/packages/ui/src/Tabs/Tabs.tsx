186f3f14cc32ab38b50f317e7dc955b256c14bb0
Sergei Droganov <serge@droganov.ru>
2020-02-11 21:57:23 +0700

    Fix tab overline attribute

--- src/packages/ui/src/Tabs/Tabs.tsx
+++ src/packages/ui/src/Tabs/Tabs.tsx
@@ -8,10 +8,13 @@ import { Props } from './Tabs.type';
 import { useIndicator } from './useIndicator';
 
 export const Tabs: FC<Props> = forwardRef<HTMLElement, Props>(
-  ({ children, color = 'primary', index, ...rest }: Props, externalRef): JSX.Element => {
+  ({ children, color = 'primary', index, overline, ...rest }: Props, externalRef): JSX.Element => {
     const [ref, indicator] = useIndicator(index, externalRef as RefObject<HTMLElement>);
     const { applyStyles } = useProviderContext();
-    const [nextProps, Element] = applyStyles({ element: 'nav', ...rest, indicator, color }, 'Tabs');
+    const [nextProps, Element] = applyStyles(
+      { element: 'nav', ...rest, indicator, color, overline },
+      'Tabs',
+    );
     return (
       <Element {...nextProps} ref={ref}>
         {children}
