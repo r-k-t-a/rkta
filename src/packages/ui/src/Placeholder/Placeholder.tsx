8c9d1a7ab39eafb8b4b9412137550fe396e1927e
Sergei Droganov <serge@droganov.ru>
2020-02-19 15:27:09 +0700

    Improve placeholder args

--- src/packages/ui/src/Placeholder/Placeholder.tsx
+++ src/packages/ui/src/Placeholder/Placeholder.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react';
 import { Props } from './Placeholder.type';
-import { makePlaceHolder } from '../util';
+import { placeHolder } from '../util';
 
 export const Placeholder: FC<Props> = ({ children, size = 3, symbol }: Props): JSX.Element => {
-  const result = children === undefined ? makePlaceHolder(symbol)(size) : children;
+  const result = children === undefined ? placeHolder(size, symbol) : children;
   return result as JSX.Element;
 };
 
