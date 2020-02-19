f7595019464a676b9073dff7144e34cd14f0db21
Sergei Droganov <serge@droganov.ru>
2020-02-19 15:28:41 +0700

    bugfix

--- src/packages/ui/src/Placeholder/Placeholder.tsx
+++ src/packages/ui/src/Placeholder/Placeholder.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react';
 import { Props } from './Placeholder.type';
-import { placeHolder } from '../util';
+import { placeholder } from '../util';
 
 export const Placeholder: FC<Props> = ({ children, size = 3, symbol }: Props): JSX.Element => {
-  const result = children === undefined ? placeHolder(size, symbol) : children;
+  const result = children === undefined ? placeholder(size, symbol) : children;
   return result as JSX.Element;
 };
 
--- src/packages/ui/src/util/placeholder.ts
+++ src/packages/ui/src/util/placeholder.ts
@@ -1,6 +1,6 @@
 import range from 'lodash/range';
 
-export const placeHolder = (length: number, character?: string): string =>
+export const placeholder = (length: number, character?: string): string =>
   range(0, length)
     .map(() => character || '\u258A')
     .join('\u200A');
