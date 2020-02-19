f77cbb1b789c4d1213b480ba26d95f0ce110721f
droganov <serge@droganov.ru>
2020-02-19 15:05:47 +0700

    Fix placeholder typings

--- src/packages/ui/src/Placeholder/Placeholder.tsx
+++ src/packages/ui/src/Placeholder/Placeholder.tsx
@@ -1,11 +1,10 @@
-import { ReactNode } from 'react';
-
+import { FC } from 'react';
 import { Props } from './Placeholder.type';
 import { makePlaceHolder } from '../util';
 
-export const Placeholder = ({ children, size = 3, symbol }: Props): ReactNode => {
-  if (children === undefined) return makePlaceHolder(symbol)(size);
-  return children as JSX.Element;
+export const Placeholder: FC<Props> = ({ children, size = 3, symbol }: Props): JSX.Element => {
+  const result = children === undefined ? makePlaceHolder(symbol)(size) : children;
+  return result as JSX.Element;
 };
 
 Placeholder.defaultProps = {
