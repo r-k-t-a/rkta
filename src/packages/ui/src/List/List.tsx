28d883edcf0dd03d95517473fa5fee9f89309755
Sergei Droganov <serge@droganov.ru>
2020-02-21 22:54:52 +0700

    Add forwardRef to List

--- src/packages/ui/src/List/List.tsx
+++ src/packages/ui/src/List/List.tsx
@@ -1,13 +1,18 @@
 /** @jsx jsx */
-import { SFC, ReactElement } from 'react';
+import { SFC, ReactElement, forwardRef } from 'react';
 import { jsx } from '@emotion/core';
 
 import { Props } from './List.type';
 import { useProviderContext } from '../Provider';
 
-// eslint-disable-next-line react/prop-types
-export const List: SFC<Props> = ({ children, ...rest }): ReactElement => {
-  const { applyStyles } = useProviderContext();
-  const [nextProps, Element] = applyStyles(rest, 'List', 'Paper', 'Text');
-  return <Element {...nextProps}>{children}</Element>;
-};
+export const List: SFC<Props> = forwardRef<HTMLElement, Props>(
+  ({ children, ...rest }: Props, ref): ReactElement => {
+    const { applyStyles } = useProviderContext();
+    const [nextProps, Element] = applyStyles(rest, 'List', 'Paper', 'Text');
+    return (
+      <Element {...nextProps} ref={ref}>
+        {children}
+      </Element>
+    );
+  },
+);
