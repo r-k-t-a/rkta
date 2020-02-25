cec805325b4022161e02166691b6319b538ac7f5
pavel.brylev <pavel.brylev@supremedesign.ru>
2020-02-25 09:34:08 +0300

    switchedOn -> enableSrcSet

--- src/packages/ui/src/Image/Image.tsx
+++ src/packages/ui/src/Image/Image.tsx
@@ -7,17 +7,17 @@ import { Props } from './Image.type';
 export const Image: FC<Props> = forwardRef<HTMLImageElement, Props>(
   ({ alt, srcSet, ...rest }: Props, externalRef): JSX.Element => {
     const internalRef = useRef<HTMLImageElement>(null);
-    const [switchedOn, setSwitchedOn] = useState(false);
+    const [enableSrcSet, setEnableSrcSet] = useState(false);
     const ref = (externalRef as RefObject<HTMLImageElement>) || internalRef;
     const intersectionRation = useIntersectionObserver(ref.current as Element);
     const { applyStyles } = useProviderContext();
     const [nextProps, Element] = applyStyles({ element: 'img', ...rest, ref }, 'Image');
 
     useEffect(() => {
-      if (switchedOn || !intersectionRation) return;
-      setSwitchedOn(true);
+      if (enableSrcSet || !intersectionRation) return;
+      setEnableSrcSet(true);
     }, [intersectionRation]);
 
-    return <Element {...nextProps} alt={alt} srcSet={switchedOn ? srcSet : undefined} />;
+    return <Element {...nextProps} alt={alt} srcSet={enableSrcSet ? srcSet : undefined} />;
   },
 );
