fa7b7f00e094493e13cbd4ef28477f9a94997a18
Sergei Droganov <serge@droganov.ru>
2020-02-26 12:05:09 +0700

    [WIP] Add suggest to Input

--- src/packages/ui/src/Input/Input.type.ts
+++ src/packages/ui/src/Input/Input.type.ts
@@ -7,6 +7,7 @@ import { Props as InputBase, InputElement } from '../InputBase/InputBase.type';
 export interface Props extends InputBase, PaperOwnProps, HTMLProps<InputElement> {
   active?: never;
   append?: ReactNode;
+  children?: (value: string, setValue?: Function) => JSX.Element;
   color?: Color;
   defaultValue?: string;
   disabled?: boolean;
