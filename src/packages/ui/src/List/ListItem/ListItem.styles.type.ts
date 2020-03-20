03ee0a9a21f0ec4103724950e81a8a2f1abd7ab9
Sergei Droganov <serge@droganov.ru>
2020-03-20 23:44:22 +0300

    add overline and underline props to list item

--- src/packages/ui/src/List/ListItem/ListItem.styles.type.ts
+++ src/packages/ui/src/List/ListItem/ListItem.styles.type.ts
@@ -1,7 +1,9 @@
 import { SerializedStyles } from '@emotion/core';
-import { CssRkta } from '../../Provider/theme/types';
+import { RktaTheme, CssRkta } from '../../Provider/theme';
 
 export interface ListItemStyles {
   initialStyle: SerializedStyles;
+  overline: (theme: RktaTheme) => SerializedStyles;
+  underline: (theme: RktaTheme) => SerializedStyles;
   [key: string]: CssRkta;
 }
