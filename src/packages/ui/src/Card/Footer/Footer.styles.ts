b7328e87125c93013d912ee7f60e9f1e0d59cc3e
Sergei Droganov <serge@droganov.ru>
2020-03-24 16:27:50 +0300

    Add more justify options to card footer

--- src/packages/ui/src/Card/Footer/Footer.styles.ts
+++ src/packages/ui/src/Card/Footer/Footer.styles.ts
@@ -3,12 +3,29 @@ import { css } from '@emotion/core';
 export const initialStyle = css`
   align-items: center;
   display: flex;
+  justify-content: start;
   padding: 8px;
   > * + * {
     margin-left: 8px;
   }
 `;
 
-export const right = css`
+export const center = css`
+  justify-content: center;
+`;
+
+export const flexEnd = css`
   justify-content: flex-end;
+`;
+
+export const spaceAround = css`
+  justify-content: space-around;
+`;
+
+export const spaceBetween = css`
+  justify-content: space-between;
+`;
+
+export const spaceEvenly = css`
+  justify-content: space-evenly;
 `;
