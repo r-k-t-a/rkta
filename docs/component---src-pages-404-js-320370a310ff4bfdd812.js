27fe62692b9ed8adec8746af81068c79cd47fa83
Sergei Droganov <serge@droganov.ru>
2020-03-25 14:09:54 +0300

    no docs in master

--- docs/component---src-pages-404-js-320370a310ff4bfdd812.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.d)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-320370a310ff4bfdd812.js.map
--- docs/styles-20483a4b482af1b8f92c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[43],{oKgo:function(o,n,w){}}]);
-//# sourceMappingURL=styles-20483a4b482af1b8f92c.js.map
