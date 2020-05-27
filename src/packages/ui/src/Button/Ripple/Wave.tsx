75e1533b9d0ec9562a601119cfaa026ddb08cf57
Павел Брылев <pavel.brylev@supremedesign.ru>
2020-05-27 17:34:03 +0300

    fix: ban types Function (#187)

--- src/packages/ui/src/Button/Ripple/Wave.tsx
+++ src/packages/ui/src/Button/Ripple/Wave.tsx
@@ -3,12 +3,11 @@ import { ReactElement, SFC } from 'react';
 import { jsx } from '@emotion/core';
 
 import { WaveCss } from './Ripple.styles';
-import { Wave as WaveCommon } from './Ripple.type';
+import { Props as RippleProps, Wave as WaveCommon } from './Ripple.type';
 import { getElement } from '../../Provider/getElement';
 
 interface Props extends WaveCommon {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  onWaveDissolve: Function;
+  onWaveDissolve: RippleProps['onWaveDissolve'];
 }
 
 export const Wave: SFC<Props> = ({ onWaveDissolve, ...wave }: Props): ReactElement => {
