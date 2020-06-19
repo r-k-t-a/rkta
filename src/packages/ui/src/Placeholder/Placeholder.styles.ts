1be6170f300830784f6d9754f6ecf7b877b92a61
Sergei Droganov <serge@droganov.ru>
2020-06-19 13:20:29 +0300

    improve placeholder animations

--- src/packages/ui/src/Placeholder/Placeholder.styles.ts
+++ src/packages/ui/src/Placeholder/Placeholder.styles.ts
@@ -1,15 +1,28 @@
-import { css, SerializedStyles } from '@emotion/core';
+import { css, keyframes, SerializedStyles } from '@emotion/core';
 import { RktaTheme } from '../Provider/theme';
 import { Props } from './Placeholder.type';
 
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0.48;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
 export const initialStyle = css`
+  animation: ${fadeIn} 4.4s ease 0.32s infinite;
   background-color: currentColor;
   border-radius: 3px;
   display: inline-block;
   pointer-events: none;
   height: 1em;
   user-select: none;
-  opacity: 0.48;
+  opacity: 0;
   width: 100%;
 `;
 
