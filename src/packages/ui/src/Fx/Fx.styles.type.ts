1c1ffa78081f48498dae008f5eaefc3ea75c2070
Sergey Pavlov <houston.inc@yandex.ru>
2020-06-25 11:45:46 +0200

    get rid of styles type

--- src/packages/ui/src/Fx/Fx.styles.type.ts
+++
@@ -1,9 +0,0 @@
-import { SerializedStyles } from '@emotion/core';
-import { CssRkta } from '../Provider/theme/types';
-
-export type FxStyles = {
-  initialStyle: SerializedStyles;
-  fadeDown: SerializedStyles;
-  popUp: SerializedStyles;
-  [key: string]: CssRkta;
-};
