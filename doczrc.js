176920c0630d0d437f588cb5fed82ccb5287c8ca
Sergei Droganov <serge@droganov.ru>
2020-06-26 20:53:13 +0300

    configure menu

--- doczrc.js
+++ doczrc.js
@@ -1,9 +1,16 @@
 export default {
   base: '/rkta',
-  debug: true,
   dest: 'docs',
   hashRouter: true,
   src: 'src/packages',
-  title: 'R•K•T•A',
+  title: 'R•K•TA',
   typescript: true,
+  ignore: ['README.md'],
+  menu: [
+    'Getting started',
+    // {
+    //   name: 'Examples',
+    //   menu: ['event', 'ref', 'on & off', 'multiple events', 'preventDefault'],
+    // },
+  ],
 };
