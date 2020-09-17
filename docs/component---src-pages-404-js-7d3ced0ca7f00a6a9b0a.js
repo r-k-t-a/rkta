65f68bd865d3f562a62694c2b471f484bf890ec4
droganov <serge@droganov.ru>
2020-09-17 21:29:54 +0300

    build docs

---
+++ docs/component---src-pages-404-js-7d3ced0ca7f00a6a9b0a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[50],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.jsx)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7d3ced0ca7f00a6a9b0a.js.map
