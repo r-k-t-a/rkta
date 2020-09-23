f9262d28e3afe0775e844a2bc9d4cc41474c15ad
droganov <serge@droganov.ru>
2020-09-23 20:13:02 +0300

    Fix Drawer type

--- src/packages/ui/src/Drawer/Drawer.type.ts
+++ src/packages/ui/src/Drawer/Drawer.type.ts
@@ -1,7 +1,7 @@
 import { ModalProps } from '../Modal/Modal.type';
 import { PaperProps } from '../Paper/Paper.type';
 
-export type DrawerProps = ModalProps &
+export type DrawerProps = Omit<ModalProps, 'visible'> &
   PaperProps & {
     align: 'left' | 'top' | 'right' | 'bottom';
     open: boolean;
