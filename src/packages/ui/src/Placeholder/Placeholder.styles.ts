f1cd7cd6ba751894583c883b7dd55ac65e1c22c4
droganov <serge@droganov.ru>
2020-10-21 17:04:10 +0300

    feat: multiline placeholders (#232)

--- src/packages/ui/src/Placeholder/Placeholder.styles.ts
+++ src/packages/ui/src/Placeholder/Placeholder.styles.ts
@@ -4,30 +4,43 @@ import { PlaceholderProps } from './Placeholder.type';
 
 const fadeIn = keyframes`
   0% {
-    opacity: 0;
+    opacity: 0.08;
   }
   50% {
     opacity: 0.48;
   }
   100% {
-    opacity: 0;
+    opacity: 0.08;
   }
 `;
 
 export const initialStyle = css`
+  align-items: center;
   animation: ${fadeIn} 4.4s ease 0.32s infinite;
-  background-color: currentColor;
-  border-radius: 3px;
-  display: block;
+  display: flex;
   pointer-events: none;
-  height: 1em;
+  position: relative;
   user-select: none;
   opacity: 0;
   width: 100%;
+  &:before {
+    content: '|';
+    color: transparent;
+    overflow: hidden;
+    width: 0;
+  }
+  &:after {
+    content: '';
+    background-color: currentColor;
+    border-radius: 3px;
+    display: block;
+    flex: 1;
+    height: 1em;
+  }
 `;
 
 export const inline = css`
-  display: inline-block;
+  display: inline-flex;
 `;
 
 export const width = (theme: RktaTheme, props: PlaceholderProps): SerializedStyles => css`
