79b1409b70bba8c6bd61ea13d200b11d646ff210
droganov <serge@droganov.ru>
2020-10-25 00:02:29 +0300

    buld docz

---
+++ docs/component---src-pages-404-js-fa810c7143d9e5e10cba.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[53],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.jsx)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-fa810c7143d9e5e10cba.js.map
